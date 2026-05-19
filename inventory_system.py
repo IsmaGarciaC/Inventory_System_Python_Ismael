784b2c0179a38744fa797d1cf32513ce3480b79c
IsmaGarciaC <melgarciacas@gmail.com>
2026-05-19 17:51:37 -0500

    Creating the product class v.1

--- inventory_system.py
+++ inventory_system.py
@@ -1,1 +1,23 @@
-print("Hello, World!")
+class Product:
+    def __init__(self, name, price, quantity):
+        self.validate_data(name, price, quantity)
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def validate_data(self, name, price, quantity):
+        if not isinstance(name, str) or not isinstance(price, float) or not isinstance(quantity, int):
+            raise ValueError("Invalid input types. Name should be a string, price should be a float, and quantity should be an integer.")
+        if name == "":
+            raise ValueError("The product name cannot be empty.")
+        if price < 0:
+            raise ValueError("Price should be non-negative.")
+        if quantity < 0:
+            raise ValueError("Quantity should not be negative.")
+
+    def __str__(self):
+        return f"{self.name} - ${self.price} (Available: {self.quantity})"
+
+    def update_price(new_price):
+	 
+
